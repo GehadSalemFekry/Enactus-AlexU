b1084804b622ca5dc3cf8a9db420bd68ebef47cd
Gehad Salem <gehad.20334@stemalex.moe.edu.eg>
2021-01-23 01:27:17 +0200

    add asset in main js

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -4,4 +4,9 @@ import './plugins/bootstrap-vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
+// asset import
+import "./assets/scss/material-dashboard.scss";
+
+
 createApp(App).mount('#app')
