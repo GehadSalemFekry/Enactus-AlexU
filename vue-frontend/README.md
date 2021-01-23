f97f2e64aa2e1f35e7c404d9249e7007eb1d5a9b
Gehad Salem <gehad.20334@stemalex.moe.edu.eg>
2021-01-23 02:24:39 +0200

    bootstrap with vue

--- vue-frontend/README.md
+++ vue-frontend/README.md
@@ -1,4 +1,4 @@
-# vue-frontend
+# testapp
 
 ## Project setup
 ```
--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -1,8 +1,14 @@
-import { createApp } from 'vue'
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
-
 
 // asset imports
 import "./assets/scss/main.scss";
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
