49056cc19f2497791c23a011e42896837b9a6670
Gehad Salem <gehad.20334@stemalex.moe.edu.eg>
2021-02-09 01:32:37 +0200

    article edit

--- vue-frontend/src/pages/ArticlesEdit.vue
+++ vue-frontend/src/pages/ArticlesEdit.vue
@@ -6,69 +6,73 @@
         <input type="text" placeholder="New Ar Article" v-model="newArArticle">
         <input type="submit" value="Create new Article" @click="addArticle">
 
-        <Article v-for="(article, index) in this.$store.state.articles" :key="article.id" :article="article" :index="index">
-        </Article>
-        <ArticleAr v-for="(article, index) in this.$store.state.articles" :key="article.id" :article="article" :index="index">
-        </ArticleAr>
+        <div>
+            <Article v-for="(article, index) in this.$store.state.articles" 
+            :key="article.id" :article="article" :index="index"></Article>
+            
+            <ArticleAr v-for="(article, index) in this.$store.state.articles" 
+            :key="article.id" :article="article" :index="index"></ArticleAr>
 
-        <ArticleDelete v-for="(article, index) in this.$store.state.articles" :key="article.id" :article="article" :index="index"></ArticleDelete>
-
+            <ArticleDelete v-for="(article, index) in this.$store.state.articles" 
+            :key="article.id" :article="article" :index="index"></ArticleDelete>
+        </div>
+        
     </div>
 </template>
 
 <script>
-
-import Article from '../components/Article'
-import ArticleAr from '../components/ArticleAr'
-import ArticleDelete from '../components/ArticleDelete'
-
+import Article from "../components/Article";
+import ArticleAr from "../components/ArticleAr";
+import ArticleDelete from "../components/ArticleDelete";
 
 export default {
-    components: {
-        Article,
-        ArticleAr,
-        ArticleDelete,
-    },
-    data() {
-        return {
-            newEnTitle: '',
-            newEnArticle: '',
-            newArTitle: '',
-            newArArticle: '',
-            // idForArticle: 1,
-            enTitlebeforeEditCache: '',
-            enBodybeforeEditCache: '',
-        }
-    },
-    
-    created() {
-        this.$store.dispatch('retrieveArticles')
-    },
+  components: {
+    Article,
+    ArticleAr,
+    ArticleDelete,
+  },
+  data() {
+    return {
+      newEnTitle: "",
+      newEnArticle: "",
+      newArTitle: "",
+      newArArticle: "",
+      // idForArticle: 1,
+      enTitlebeforeEditCache: "",
+      enBodybeforeEditCache: "",
+    };
+  },
 
-    methods: {
-        addArticle() {
-            if (this.newEnTitle.trim().length == 0 || this.newEnArticle.trim().length == 0) {
-                return
-            }
+  created() {
+    this.$store.dispatch("retrieveArticles");
+  },
 
-            this.$store.dispatch('addArticle', {
-                //id: this.idForArticle,
-                en_title: this.newEnTitle,
-                en_body: this.newEnArticle,
-                ar_title: this.newArTitle,
-                ar_body: this.newArArticle,
-            })
+  methods: {
+    addArticle() {
+      if (
+        this.newEnTitle.trim().length == 0 ||
+        this.newEnArticle.trim().length == 0
+      ) {
+        return;
+      }
 
-            this.newEnTitle = ''
-            this.newEnArticle = ''
-            this.newArTitle = ''
-            this.newArArticle = ''
-            //this.idForArticle++
-        }
+      this.$store.dispatch("addArticle", {
+        //id: this.idForArticle,
+        en_title: this.newEnTitle,
+        en_body: this.newEnArticle,
+        ar_title: this.newArTitle,
+        ar_body: this.newArArticle,
+      });
+
+      this.newEnTitle = "";
+      this.newEnArticle = "";
+      this.newArTitle = "";
+      this.newArArticle = "";
+      //this.idForArticle++
     },
-}
+  },
+};
 </script>
 
 <style lang="en">
-    
 </style>
